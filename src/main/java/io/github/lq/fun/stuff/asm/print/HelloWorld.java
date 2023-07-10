cd3efb482cd2689f235bf2a75e6b28b687778a87
qi.li <15868175516@163.com>
2023-07-10 21:37:42 +0800

    feat(asm): frame

--- src/main/java/io/github/lq/fun/stuff/asm/print/HelloWorld.java
+++ src/main/java/io/github/lq/fun/stuff/asm/print/HelloWorld.java
@@ -4,4 +4,13 @@ public class HelloWorld {
     public void test() {
         System.out.println("Test Method");
     }
+
+    public void branch(boolean flag) {
+        if (flag) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+    }
 }
